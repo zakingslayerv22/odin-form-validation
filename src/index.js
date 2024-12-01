f90cbb15a24ca7862e34b54fedd1e7b9e8c755d9
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2024-12-01 04:01:06 -0800

    Create country and zip validation condition
    First select the country and
    zipcode fields in the constructor.
    Create a constraints object that
    contains zip code regular
    expressions pattern and
    validity messages for each
    corresponding country
    (denoted in the object by the
    country's corresponding
    value in the select field.)
    
    Create a currentConstraint
    property that creates a
    Regular Expression out of
    the pattern in the objects'
    arrays first element. This is
    capture Switzerland because
    it is the first option in
    the country select field.
    
    Initialize a method,
    validateCountryAndZipCondition().
    The method first saves the
    result of the test of the
    value of the zipcode field
    against the currentConstraint
    pattern to a constant,
    matchesPattern.
    If it matches the pattern,
    it sets the custom validity message
    to an empty string else it sets the
    validity message to the second element
    of the array of the corresponding
    country.
    
    It then returns matchesPattern.
    
    This method will be called when
    the country select field changes
    and when the zipcode field loses
    focus so that country and
    zipcode can be validated.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import "./index.css";
 class ValidateForm {
   constructor() {
     this.form = document.querySelector("form");
+    this.countrySelectField = document.querySelector("#country");
+    this.zipcodeInputField = document.querySelector("#zipcode");
+
+    // For each country, defines the pattern that the postal code has to follow
+    this.constraints = {
+      ch: [
+        "^(CH-)?\\d{4}$",
+        "Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
+      ],
+      fr: [
+        "^(F-)?\\d{5}$",
+        "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+      ],
+      de: [
+        "^(D-)?\\d{5}$",
+        "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+      ],
+      nl: [
+        "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+        "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+      ],
+    };
+
+    this.currentConstraint = new RegExp(
+      this.constraints[this.countrySelectField.value][0],
+    );
 
     if (this.form) this.initialize();
   }
@@ -32,6 +58,22 @@ class ValidateForm {
 
       emailInput.reportValidity();
     });
+  }
+
+  validateCountryAndZipCondition() {
+    const matchesPattern = this.currentConstraint.test(
+      this.zipcodeInputField.value,
+    );
+
+    if (matchesPattern) {
+      this.zipcodeInputField.setCustomValidity("");
+    } else {
+      this.zipcodeInputField.setCustomValidity(
+        `${this.constraints[this.countrySelectField.value][1]}`,
+      );
+    }
+
+    return matchesPattern;
   }
 }
 
